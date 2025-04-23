dd074fb1dbc3415b7bc7a5f3c5ab092d474019a9
Wyatt Morgan <70215848+WyattMorgan20@users.noreply.github.com>
2025-04-23 12:45:54 -0500

    Added DogsManagementSystem to new repo

---
+++ DogsManagementSystem/src/main/java/com/nwmsu/DMS/repository/TrainerRepository.java
@@ -0,0 +1,11 @@
+package com.nwmsu.DMS.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.nwmsu.DMS.Models.Trainer;
+
+public interface TrainerRepository extends CrudRepository<Trainer, Integer> {
+
+	
+	
+}
